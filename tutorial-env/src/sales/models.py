a97794d9caef00bdb7e0962f5d08ef7c66211654
zlima410 <endercraft68@gmail.com>
2024-02-27 20:32:33 -0500

    Add Position and Sale models to sales app

--- tutorial-env/src/sales/models.py
+++ tutorial-env/src/sales/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
+
+class Position(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.FloatField(blank = True)
+    created = models.DateTimeField(blank = True)
+
+    def save(self, *args, **kwargs):
+        self.price = self.product.price * self.quantity
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"id: {self.id}, product: {self.product.name}, quantity: {self.quantity}"
+
+class Sale(models.Model):
+    pass
+
+class CSV(models.model):
+    pass
