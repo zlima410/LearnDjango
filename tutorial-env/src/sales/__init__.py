e9a1aa7d844f4d2447c34ddee2d69b22df94affd
zlima410 <endercraft68@gmail.com>
2024-02-27 21:22:21 -0500

    Add total price calculation in Sale class using signals.py

---
+++ tutorial-env/src/sales/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'Sales.apps.SalesConfig'
--- tutorial-env/src/sales/models.py
+++ tutorial-env/src/sales/models.py
@@ -23,21 +23,21 @@ class Position(models.Model):
 class Sale(models.Model):
     transaction_id = models.CharField(max_length = 12, blank = True)
     positions = models.ManyToManyField(Position)
-    total_price = models.FloatField(blank = True)
+    total_price = models.FloatField(blank = True, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     salesman = models.ForeignKey(Profile, on_delete = models.CASCADE)
     created = models.DateTimeField(blank = True)
     updated = models.DateTimeField(auto_now = True)
 
     def __str__(self):
-        return f"Sales for the amount of ${self.total_price}"
+        return f"Sale for the amount of ${self.total_price}"
     
     def save(self, *args, **kwargs):
         if self.transaction_id == "":
-            self.transaction_id == generate_code()
+            self.transaction_id = generate_code()
         if self.created is None:
             self.created = timezone.now()
-            return super().save(*args, **kwargs)
+        return super().save(*args, **kwargs)
         
     def get_positions(self):
         return self.positions.all()
